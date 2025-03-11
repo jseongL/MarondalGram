608770a5164cb43467698b783f78060a0e634584
jsL <imi90655@naver.com>
2025-03-11 21:02:15 +0900

    [임지성] -MarondalGram 프로젝트 생성

---
+++ src/main/java/com/jsL/MarondalGram/MarondalGramApplication.java
@@ -0,0 +1,13 @@
+package com.jsL.MarondalGram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarondalGramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarondalGramApplication.class, args);
+	}
+
+}
